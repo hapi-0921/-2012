6aba3a845c8a11b08ebcf6a5ae00cded63e599d8
MSI\2250049 <a>
2026-04-27 12:22:05 +0900

    titleにてクリックで次に進めるようにするための準備

--- SourceCode/player.cpp
+++ SourceCode/player.cpp
@@ -1,10 +1,23 @@
-#include "player.h"
 #include "../GameLib/game_lib.h"
+#include "player.h"
+#include "SceneTitle.h"
+
+
+Button startButton = { 300,200,200,50,0 };
+Button howtoButton = { 300,280,200,50,1 };
+
+int player_timer;
+static bool prevMouseLeft;
 
 Player::Player()
 {
 	 cursorX = 0;
 	 cursorY = 0;
+
+	 cursorIndex = 0;
+	 decided = false;
+
+	 player_timer = 0;
 }
 
 Player::~Player()
@@ -20,12 +33,43 @@ CursorPos Player::getCursorpos()
 	 return{ cursorX,cursorY };
 }
 
-void Player::player_update()
+bool Player::IsHovered(Button button, float mouseX, float mouseY)
 {
-
+	return mouseX >= button.x && mouseX <= button.x + button.width &&
+		 mouseY >= button.y && mouseY <= button.y + button.height;
 }
 
-void Player::player_render()
+void Player::MenuUpdate()
 {
+	player_timer++;
 
+	CursorPos pos = getCursorpos();
+
+	if (TRG(0)&PAD_DOWN) cursorIndex++;
+	if (TRG(0)&PAD_UP) cursorIndex--;
+	
+	if (cursorIndex < 0)cursorIndex = startButton.index;
+	if (cursorIndex > 1)cursorIndex = howtoButton.index;
+	if (IsHovered(startButton, pos.x, pos.y)) cursorIndex = startButton.index;
+	if (IsHovered(howtoButton, pos.x, pos.y)) cursorIndex = howtoButton.index;
+
+	bool mouseLeft = (GetAsyncKeyState(VK_LBUTTON) & 0x8000) != 0;
+	bool mouseClick = (!prevMouseLeft && mouseLeft && player_timer > 30);
+
+	if ((TRG(0) & PAD_START) || mouseClick)
+	{
+		decided = true;
+	}
+
+	prevMouseLeft = mouseLeft;
+}
+
+bool Player::IsDecided()
+{
+	return decided;
+}
+
+int Player::GetCursorIndex()
+{
+	return cursorIndex;
 }
--- SourceCode/player.h
+++ SourceCode/player.h
@@ -8,17 +8,33 @@ struct CursorPos
 	int y;
 };
 
+struct Button
+{
+	float x;
+	float y;
+	float width;
+	float height;
+	int index;
+};
+
 class Player
 {
 private:
 	int cursorX;
 	int cursorY;
+	int cursorIndex;
 
 public:
 	Player();
 	~Player();
-	void player_update();
-	void player_render();
+
+	bool IsHovered(Button button, float mouseX, float mouseY);
+
+	int GetCursorIndex();
+	bool decided;
+	bool IsDecided();
+
+	void MenuUpdate();
 
 	CursorPos getCursorpos();
 };
